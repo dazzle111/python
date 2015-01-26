218ed171f3858b7c87277e6af5c9dfba5275c98e
dazzle111 <654504711@qq.com>
2015-01-26 18:50:08 +0800

    python backup test

---
+++ backup_ver1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import os
+import time
+
+source  = ['/home/swaroop/bin','/home/swaroop/bin']
+
+target_dir = '/mnt/e/backup'
+
+target = target_dir + time.strftime('%Y%m%d%H%M%S') + '.zip'
+zip_command = "zip -qr '%s' %s" % (target,' '.join(source))
+
+if os.system(zip_command) == 0:
+	print 'Successful backup to ',target
+else:
+	print 'Backup FAILED'
